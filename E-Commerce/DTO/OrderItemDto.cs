f8d48f14a50152950ec3ab88e1924a31877c8044
Mostafa <mostafayounis051@gmail.com>
2026-09-02 11:03:29 +0300

    made dto files

---
+++ E-Commerce/DTO/OrderItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Commerce.DTO
+{
+    public class OrderItemDto
+    {
+        public required string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ E-Commerce/DTO/OrdersDto.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Models;
+
+namespace E_Commerce.DTO
+{
+    public class OrdersDto
+    {
+        public required string CustomerEmail { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
