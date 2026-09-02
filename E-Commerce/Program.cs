56f1491573e019adbe22bbd3aa748f7f41389148
Mostafa <mostafayounis051@gmail.com>
2026-09-02 11:05:14 +0300

    program updated

--- E-Commerce/Program.cs
+++ E-Commerce/Program.cs
@@ -1,9 +1,13 @@
 using E_Commerce.Models;
+using E_Commerce.Services;
+using Hangfire;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddHangfireServer();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -12,7 +16,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddTransient<IOrderServices, OrderServices>();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -26,6 +30,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseHangfireDashboard();
 app.UseAuthorization();
 
 app.MapControllers();
