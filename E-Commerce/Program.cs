0e2fbfc8a71820f9dbdb1e2ac6908fff032b61bd
Mostafa <mostafayounis051@gmail.com>
2026-09-07 13:47:21 +0300

    added dockerfile

--- E-Commerce/Program.cs
+++ E-Commerce/Program.cs
@@ -24,12 +24,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+
     app.UseSwagger();
     app.UseSwaggerUI();
     app.UseHangfireDashboard();
-}
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
